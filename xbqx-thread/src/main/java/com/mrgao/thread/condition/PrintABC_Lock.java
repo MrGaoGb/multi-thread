48da9af8b2467b1edf200fbd35ab8b522b540c21
Mr.Gao <862230476@qq.com>
2024-08-19 17:08:47 +0800

    线程ABC按顺序执行(以Lock方式自行)

---
+++ xbqx-thread/src/main/java/com/mrgao/thread/condition/PrintABC_Lock.java
@@ -0,0 +1,78 @@
+package com.mrgao.thread.condition;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author Mr.Gao
+ * @date 2024/8/19 16:51
+ * @apiNote:
+ */
+public class PrintABC_Lock {
+
+    private static ReentrantLock lock = new ReentrantLock();
+
+    private volatile static int state = 0;
+
+    static class ThreadA extends Thread {
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; ) {
+                lock.lock();
+                try {
+                    while (state % 3 == 0) {// 多线程并发，不能用if，必须用循环测试等待条件，避免虚假唤醒
+                        System.out.print("A");
+                        state++;
+                        i++;
+                    }
+                } finally {
+                    lock.unlock();// unlock()操作必须放在finally块中
+                }
+            }
+        }
+    }
+
+    static class ThreadB extends Thread {
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; ) {
+                lock.lock();
+                try {
+                    while (state % 3 == 1) {// 多线程并发，不能用if，必须用循环测试等待条件，避免虚假唤醒
+                        System.out.print("B");
+                        state++;
+                        i++;
+                    }
+                } finally {
+                    lock.unlock();// unlock()操作必须放在finally块中
+                }
+            }
+        }
+    }
+
+    static class ThreadC extends Thread {
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; ) {
+                lock.lock();
+                try {
+                    while (state % 3 == 2) {// 多线程并发，不能用if，必须用循环测试等待条件，避免虚假唤醒
+                        System.out.print("C:" + state);
+                        state++;
+                        i++;
+                    }
+                } finally {
+                    lock.unlock();// unlock()操作必须放在finally块中
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new ThreadA().start();
+        new ThreadB().start();
+        new ThreadC().start();
+    }
+}
