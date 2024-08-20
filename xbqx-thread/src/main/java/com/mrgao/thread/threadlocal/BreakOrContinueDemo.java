99b810537719ec6b830f0bd7fb4d5f9805358fdf
MrGaoGb <862230476@qq.com>
2024-08-21 00:15:23 +0800

    break和continue以及搭配flag标识符使用

---
+++ xbqx-thread/src/main/java/com/mrgao/thread/threadlocal/BreakOrContinueDemo.java
@@ -0,0 +1,31 @@
+package com.mrgao.thread.threadlocal;
+
+/**
+ * 研究break和continue
+ * break:终止跳出该循环体
+ * continue: 终止跳出本次循环，进入下一个循环
+ *
+ * 而 break flag(标识符):终止跳出最外层循环体
+ * continue flag(标识符):终止跳出本次循环的最外层循环体，进入下一个循环
+ */
+public class BreakOrContinueDemo {
+
+    public static void main(String[] args) {
+
+        flag:
+        for(int i=0; i<10; i++) {
+            System.out.println("外部循环:"+i);
+            for(int j=0; j<10; j++){
+                System.out.println("内部循环:"+j);
+                int sum = i*j;
+                if(sum>50){
+                    System.out.println(i+"*"+j+"="+sum);
+//                    break flag;
+//                    break;
+                    continue flag;
+                }
+            }
+        }
+
+    }
+}
