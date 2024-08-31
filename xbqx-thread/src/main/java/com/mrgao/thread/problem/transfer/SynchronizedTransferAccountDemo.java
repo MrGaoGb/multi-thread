05dc9cb10661e74a268cbd9dfe7e5f1db022a3dc
MrGaoGb <862230476@qq.com>
2024-08-31 18:54:57 +0800

    CountDownLatch解决前面三种锁失效的问题

--- xbqx-thread/src/main/java/com/mrgao/thread/problem/transfer/SynchronizedTransferAccountDemo.java
+++ xbqx-thread/src/main/java/com/mrgao/thread/problem/transfer/SynchronizedTransferAccountDemo.java
@@ -1,6 +1,12 @@
 package com.mrgao.thread.problem.transfer;
 
 
+import org.springframework.util.StopWatch;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @Description TODO
  * @Author Mr.Gao
@@ -13,13 +19,16 @@ public class SynchronizedTransferAccountDemo {
     public static void main(String[] args) throws InterruptedException {
 //        CountDownLatch countDownLatch = new CountDownLatch(2);
         SynchronizedTransferAccountDemo transferAccountDemo = new SynchronizedTransferAccountDemo();
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        System.out.println("开始时间:" + LocalDateTime.now());
         // 处理无限循环中
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < 100000; i++) {
 
             // 账户A、B、C账户余额分别为200, A给B转账100 然后B给C转账100，最后输出各自余额还剩多少
 
             // 第一种引出问题
-            boolean execResult = transferAccountDemo.planOne();
+//            boolean execResult = transferAccountDemo.planOne();
 
             // 使用同一把锁锁定待操作的资源
             //boolean execResult = transferAccountDemo.planTwo();
@@ -27,12 +36,15 @@ public class SynchronizedTransferAccountDemo {
             // 使用同一把锁锁定待操作的资源
             //boolean execResult = transferAccountDemo.planThree();
 
+            // 结合CountDownLatch等线程执行完之后再继续执行
+            boolean execResult = transferAccountDemo.planFour();
 
             if (execResult) {
                 break;
             }
-
         }
+        stopWatch.stop();
+        System.out.println("开始时间:" + LocalDateTime.now() + ",耗时:" + stopWatch.getTotalTimeMillis() + "ms!");
     }
 
 
@@ -101,7 +113,6 @@ public class SynchronizedTransferAccountDemo {
         System.out.println("输出A余额(转账前):" + A.getBalance());
         System.out.println("输出B余额(转账前):" + B.getBalance());
         System.out.println("输出C余额(转账前):" + C.getBalance());
-
         // A转账B 转账100元
         Thread threadA = new Thread(() -> {
             System.out.println(Thread.currentThread().getName() + ",开始发起转账操作!");
@@ -203,6 +214,64 @@ public class SynchronizedTransferAccountDemo {
         }
 
         return false;
+    }
+
+    /**
+     * 第四种方式优化
+     * <p>
+     * 通过CountDownLatch解决在线程执行完毕后，再进行打印结果，原因是通过调用start方法之后，不能确保线程确实已经被执行完了，只有线程本身真正执行完毕后，
+     * 即就是以最后的结果为准
+     * </p>
+     */
+    public boolean planFour() {
+        //账户A、B、C账户余额分别为200, A给B转账100 然后B给C转账100，最后输出各自余额还剩多少
+        CountDownLatch a2bCountDownLatch = new CountDownLatch(1);
+        CountDownLatch b2cCountDownLatch = new CountDownLatch(1);
+
+        TransferAccount A = new TransferAccount(200);
+        TransferAccount B = new TransferAccount(200);
+        TransferAccount C = new TransferAccount(200);
+
+        // A转账B 转账100元
+        Thread threadA = new Thread(() -> {
+            A.transferUpgrade(B, 100);// 存在并发问题
+            a2bCountDownLatch.countDown();
+        }, "threadA");
+
+        // B转账C 转账100元
+        Thread threadB = new Thread(() -> {
+            B.transferUpgrade(C, 100);// 存在并发问题
+            b2cCountDownLatch.countDown();
+        }, "threadB");
+
+
+        // 启动线程A、B
+        threadA.start();
+        threadB.start();
+
+        // 等待线程A执行完
+        try {
+            a2bCountDownLatch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        // 等待线程B执行完
+        try {
+            b2cCountDownLatch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // 异常并发情况
+        if (A.getBalance().equals(100) && B.getBalance().equals(200) && C.getBalance().equals(300)) {
+//            String printVal = String.format("A->B->C 转账成功: A的余额为:%d , B的余额为:%d,C的余额为:%d", A.getBalance(), B.getBalance(), C.getBalance());
+//            System.out.println(Thread.currentThread().getName() + " " + printVal);
+            return false;
+        } else {
+            String printVal = String.format("A->B->C 转账失败: A的余额为:%d , B的余额为:%d,C的余额为:%d", A.getBalance(), B.getBalance(), C.getBalance());
+            System.out.println(Thread.currentThread().getName() + ": " + printVal);
+            return true;
+        }
     }
 
 }
