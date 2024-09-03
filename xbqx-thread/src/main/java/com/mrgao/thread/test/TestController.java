f15902eeba907d91025564746959bee82c263498
MrGaoGb <862230476@qq.com>
2024-09-04 01:10:51 +0800

    自定义返回responseStatus状态码

---
+++ xbqx-thread/src/main/java/com/mrgao/thread/test/TestController.java
@@ -0,0 +1,28 @@
+package com.mrgao.thread.test;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description TODO
+ * @Author Mr.Gao
+ * @Date 2024/9/3 22:38
+ */
+@RestController
+public class TestController {
+
+
+    @GetMapping(value = "/test/funOne", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String funOne() {
+        return "fun one";
+    }
+
+    @GetMapping("/test/funTwo")
+    public ResponseEntity funTwo() {
+        ResponseEntity responseEntity = new ResponseEntity("fun two", HttpStatus.BAD_REQUEST);
+        return responseEntity;
+    }
+}
