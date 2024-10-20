9e0a57cef51e3bdb780e255a251327ef81ba3902
Mr.Gao <862230476@qq.com>
2024-10-20 21:22:16 +0800

    Synchronized运行原理

---
+++ xbqx-thread/src/main/java/com/mrgao/thread/base/thread/ForeverLoopDemo.java
@@ -0,0 +1,45 @@
+package com.mrgao.thread.base.thread;
+
+/**
+ * @author Mr.Gao
+ * @apiNote:
+ * @date 2024/10/20 14:37
+ */
+public class ForeverLoopDemo {
+
+    static boolean flag = false;
+
+    public static void main(String[] args) {
+
+        // 创建线程1
+        new Thread(() -> {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            flag = true;
+            System.out.println(Thread.currentThread().getName() + ",将flag变量修改为true！");
+        }, "T1").start();
+
+        // 创建线程2
+        new Thread(() -> {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(Thread.currentThread().getName() + ",获取flag变量:" + flag);
+        }, "T2").start();
+
+        // 创建线程3
+        new Thread(() -> {
+            int c = 0;
+            while (!flag) {
+                c++;
+            }
+            System.out.println(Thread.currentThread().getName() + ",输出变量c的值:" + c);
+        }, "T3").start();
+    }
+
+}
