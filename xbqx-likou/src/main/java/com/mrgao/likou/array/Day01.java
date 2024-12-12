6a94a17ca2d730af47552fcaff0d2eebca4986f3
MrGaoGb <862230476@qq.com>
2024-12-13 00:53:29 +0800

    合并两个有序数组(力扣_day01)

---
+++ xbqx-likou/src/main/java/com/mrgao/likou/array/Day01.java
@@ -0,0 +1,59 @@
+package com.mrgao.likou.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Description 合并两个有序数组
+ * @Author Mr.Gao
+ * @Date 2024/12/12 23:10
+ */
+public class Day01 {
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+//        mergeV1(nums1, 3, nums2, 3);
+
+        mergeV2(nums1, 3, nums2, 3);
+    }
+
+
+    /**
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
+    public static void mergeV1(int[] nums1, int m, int[] nums2, int n) {
+        // 元素总个数
+        int len = m + n;
+        List<Integer> list = new ArrayList<>();
+        // 循环取出所有非0元素
+        for (int i = 0; i < len; i++) {
+            if (i < nums1.length && nums1[i] != 0) {
+                list.add(nums1[i]);
+            }
+            if (i < nums2.length && nums2[i] != 0) {
+                list.add(nums2[i]);
+            }
+        }
+        // 排序
+        list.sort(Integer::compare);
+        System.out.println(list);
+        // 转化为nums1数组
+        // list 转化为int数组
+//        nums1 = list.stream().mapToInt(Integer::intValue).toArray();
+//        System.out.println(Arrays.toString(nums1));
+
+    }
+
+    public static void mergeV2(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
+        }
+        Arrays.sort(nums1);
+        System.out.println(Arrays.toString(nums1));
+    }
+}
