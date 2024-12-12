8b707ae05463f4327ce030dac3bc8a88c3e80399
MrGaoGb <862230476@qq.com>
2024-12-13 01:03:26 +0800

    合并两个有序数组(力扣_day01)

--- xbqx-likou/src/main/java/com/mrgao/likou/array/Day01.java
+++ xbqx-likou/src/main/java/com/mrgao/likou/array/Day01.java
@@ -3,6 +3,7 @@ package com.mrgao.likou.array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 /**
  * @Description 合并两个有序数组
@@ -16,7 +17,9 @@ public class Day01 {
         int[] nums2 = {2, 5, 6};
 //        mergeV1(nums1, 3, nums2, 3);
 
-        mergeV2(nums1, 3, nums2, 3);
+//        mergeV2(nums1, 3, nums2, 3);
+
+        mergeV3();
     }
 
 
@@ -44,7 +47,7 @@ public class Day01 {
         System.out.println(list);
         // 转化为nums1数组
         // list 转化为int数组
-//        nums1 = list.stream().mapToInt(Integer::intValue).toArray();
+        nums1 = list.stream().mapToInt(Integer::intValue).toArray();
 //        System.out.println(Arrays.toString(nums1));
 
     }
@@ -55,5 +58,16 @@ public class Day01 {
         }
         Arrays.sort(nums1);
         System.out.println(Arrays.toString(nums1));
+    }
+
+    public static void mergeV3() {
+
+        int sum = IntStream.of(1, 2, 3, 4,5)
+                .filter(e -> e > 2)
+                .peek(e -> System.out.println("Filtered value: " + e))
+                .map(e -> e * e)
+                .peek(e -> System.out.println("Mapped value: " + e))
+                .sum();
+        System.out.println(sum);
     }
 }
