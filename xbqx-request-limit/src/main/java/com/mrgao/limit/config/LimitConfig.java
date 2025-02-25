5b367f6ee621044b6645f1a4d4f0f7d23e295d1f
Mr.Gao <862230476@qq.com>
2025-02-25 18:14:31 +0800

    添加日志

--- xbqx-request-limit/src/main/java/com/mrgao/limit/config/LimitConfig.java
+++ xbqx-request-limit/src/main/java/com/mrgao/limit/config/LimitConfig.java
@@ -2,6 +2,7 @@ package com.mrgao.limit.config;
 
 import com.mrgao.limit.algorithm.TrafficLimiter;
 import com.mrgao.limit.algorithm.impl.CounterLimiter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
  * @apiNote:
  * @date 2025/2/25 18:03
  */
+@Slf4j
 @Configuration
 public class LimitConfig {
 
@@ -20,6 +22,7 @@ public class LimitConfig {
      */
     @Bean
     public TrafficLimiter trafficLimiter() {
+        log.info("限流算法之计数器限流配置Bean初始化!");
         return new CounterLimiter();
     }
 
