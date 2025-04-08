23ab34d5e3cb855bedd0837293d3fc76f6f3e429
Mr.Gao <862230476@qq.com>
2025-04-08 20:44:17 +0800

    根据字节码指令重新学习finally 原理

--- xbqx-thread/src/main/java/com/mrgao/thread/base/TryFinallyDemo.java
+++ xbqx-thread/src/main/java/com/mrgao/thread/base/TryFinallyDemo.java
@@ -1,23 +1,33 @@
 package com.mrgao.thread.base;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @Description TODO
  * @Author Mr.Gao
  * @Date 2025/3/30 23:47
  */
 public class TryFinallyDemo {
+
+    private static Logger logger = LoggerFactory.getLogger(TryFinallyDemo.class);
+
     public static void main(String[] args) {
         try {
-            System.out.println("try");
-            System.out.println("##########业务逻辑(Begin)############");
-            System.out.println("......");
-            System.out.println("##########业务逻辑(Begin)############");
+
+            int a = 1;
+
+            int b = 2;
+
+            int c = b / 2;
 
             // 终止JVM运行
             //System.exit(0);
 
+        } catch (Exception ex) {
+            logger.info("catch....!");
         } finally {
-            System.out.println("finally");
+            logger.info("finally.....!");
         }
     }
 }
