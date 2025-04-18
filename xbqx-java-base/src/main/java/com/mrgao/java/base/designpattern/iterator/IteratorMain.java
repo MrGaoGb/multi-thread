ccfb5a51a51b62d28304dc029be46129f30d8395
Mr.Gao <862230476@qq.com>
2025-04-18 12:03:01 +0800

    自定义User为增强forEach，并且实现UserIte的迭代器

--- xbqx-java-base/src/main/java/com/mrgao/java/base/designpattern/iterator/IteratorMain.java
+++ xbqx-java-base/src/main/java/com/mrgao/java/base/designpattern/iterator/IteratorMain.java
@@ -1,8 +1,12 @@
 package com.mrgao.java.base.designpattern.iterator;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 /**
  * @author Mr.Gao
@@ -17,29 +21,47 @@ public class IteratorMain {
     public static void main(String[] args) {
         // 创建用户集合
         List<User> userList = new ArrayList<>();
+        //// 遍历用户数据
+        //for (User user : userList) {
+        //    System.out.println(user);
+        //}
+
+        // 自定义实现迭代器
+        //User user = new User("张三", 18);
+        //for (String s : user) {
+        //    System.out.println(s);
+        //}
+
+        //Iterator<String> iterator = user.iterator();
+        //while (iterator.hasNext()) {
+        //    String next = iterator.next();
+        //    System.out.println(next);
+        //}
 
         // 向集合中添加用户信息
         userList.add(new User("张三", 18));
         userList.add(new User("李四", 18));
 
         // 遍历用户数据
-        //for (User user : userList) {
-        //    if (user.getAge() == 18) {
-        //        userList.add(new User("王五", 23));
-        //    }
-        //}
+        for (User user : userList) {
+            if (user.getAge() == 18) {
+                userList.add(new User("王五", 23));
+                break;
+            }
+        }
 
 
         // 迭代器
-        Iterator<User> iterator = userList.iterator();
-        while (iterator.hasNext()) {
-            // 获取一个元素
-            User user = iterator.next();
-            if (user.getAge() == 18) {
-                // 移除一个元素
-                userList.add(new User("赵六", 24));
-            }
-        }
+        //Iterator<User> iterator = userList.iterator();
+        //while (iterator.hasNext()) {
+        //    // 获取一个元素
+        //    User user = iterator.next();
+        //    System.out.println(user);
+        //    //if (user.getAge() == 18) {
+        //    //    // 移除一个元素
+        //    //    userList.add(new User("赵六", 24));
+        //    //}
+        //}
 
         System.out.println(userList);
 
@@ -51,26 +73,17 @@ public class IteratorMain {
 /**
  * 用户信息
  */
-class User {
+@Getter
+@Setter
+class User implements Iterable<String> {
+
+    @Override
+    public Iterator<String> iterator() {
+        return new UserIte();
+    }
 
     private String name;
     private Integer age;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 
     public User(String name, Integer age) {
         this.name = name;
@@ -83,5 +96,29 @@ class User {
                 "name='" + name + '\'' +
                 ", age=" + age +
                 '}';
+    }
+
+    class UserIte implements Iterator<String> {
+
+        private int count = 2;
+
+        @Override
+        public boolean hasNext() {
+            return count > 0;
+        }
+
+        @Override
+        public String next() {
+            count--;
+            //System.out.println("count:" + count);
+            if (count == 1) {
+                return age + "";
+            }
+            if (count == 0) {
+                return name;
+            }
+            // 没有元素之后抛出异常NoSuchElementException
+            throw new NoSuchElementException();
+        }
     }
 }
